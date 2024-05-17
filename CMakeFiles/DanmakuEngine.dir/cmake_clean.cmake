e79855e1ed66011c03138cc1ff97fcb8d126b22e
Siminischi C Alexandru Sebastian <siminischi1234@gmail.com>
2024-05-17 10:15:35 +0000

    made it build and run from scratch

---
+++ CMakeFiles/DanmakuEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DanmakuEngine.dir/src/main.cpp.o"
+  "CMakeFiles/DanmakuEngine.dir/src/main.cpp.o.d"
+  "DanmakuEngine"
+  "DanmakuEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DanmakuEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
